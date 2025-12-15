e7c98b91f4cc9533d16112ccda70f5f706be321e
SurajGuldhe <surajguldhe888@gmail.com>
2025-12-15 14:48:43 +0530

    First commit

---
+++ src/test/java/com/tka/pfd/seleniumfirst/Dayfirst.java
@@ -0,0 +1,13 @@
+package com.tka.pfd.seleniumfirst;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Dayfirst {
+	
+	public static void main(String[] args) {
+ChromeDriver driver = new ChromeDriver();
+driver.get("https://www.instagram.com/");
+	
+	}
+
+}
